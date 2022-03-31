aca3791ca0df1d34831f94198911b4a9c675268f
lishanyi-66 <lishanyi18@gmail.com>
2022-03-31 23:46:59 +0800

    003

--- case03/main.js
+++ case03/main.js
@@ -1,9 +1,36 @@
 
 window.onload=function(){
+    let arr = ['web前端','HTML核心','css核心','Vue','CSS+HTML','Javascript',]
     let input=document.getElementById('value')
-let show = document.getElementById('show')
-console.log(input)
-input.onkeyup=function(){
-    show.style.display="block"
-}
+    let show = document.getElementById('show')
+// console.log(input)
+// 键盘按下事件
+    input.onkeyup=function(){
+        show.style.display="block"
+        //  设置空元素
+        let str=''
+        arr.forEach((item)=>{
+            let res= item.indexOf(input.value)
+            // console.log(res)
+            if(res!=-1){
+                str += "<p>"+item+"</p>" ;
+                // 插入HTML元素要加上双引号
+            }
+            
+        })
+        // 如果input.value为空或者str为false 给用户提示
+        if(!input.value||!str){
+            show.innerHTML="<p>暂无结果</p>"
+        }else{
+            show.innerHTML=str
+        }
+        // console.log(str)
+        
+    }
+    
+    // 
+    input.onblur=function(){
+        show.style.display="none";
+        input.value="";
+    }
 }
